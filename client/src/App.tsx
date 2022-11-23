c8307c452d3a0b0dd253f3822e77023f44e18192
shanta <teirenxuanshanta@gmail.com>
2022-11-23 21:42:34 +0800

    added maps

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,15 +2,20 @@ import { useEffect } from "react";
 import socket from "./socket";
 import { Box, Button, Heading, HStack } from "@chakra-ui/react";
 import Form from "./components/Form";
+import Map from "./components/Map";
 
 function App() {
   useEffect(() => {
     socket;
+    
   }, []);
 
   return (
     <Box>
-      <Form />
+      <HStack>
+        {/* <Form /> */}
+        <Map />
+      </HStack>
     </Box>
   );
 }
